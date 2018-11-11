35e717545dcb597157b643850c93ac3b511e90b8
bryanl <bryanliles@gmail.com>
2018-11-11 12:08:38 -0500

    draw nodes staggered

--- src/components/dag.tsx
+++ src/components/dag.tsx
@@ -10,12 +10,18 @@ export class DAG extends React.Component<DAGProps, {}> {
   render() {
     const drawn: { [key: string]: boolean } = {};
     const nodes = [];
+    const nodeSpacing = 250;
     let childId = 0;
+
+    let xPos = 100;
 
     for (var key in this.props.dag) {
       if (!drawn[key]) {
         childId++;
-        nodes.push(<ResourceNode key={childId} label={key} />);
+        nodes.push(
+          <ResourceNode key={childId} label={key} pos={{ x: xPos, y: 100 }} />
+        );
+        xPos += nodeSpacing;
         drawn[key] = true;
       }
 
@@ -23,7 +29,14 @@ export class DAG extends React.Component<DAGProps, {}> {
         var label = this.props.dag[key][i];
         if (!drawn[label]) {
           childId++;
-          nodes.push(<ResourceNode key={childId} label={label} />);
+          nodes.push(
+            <ResourceNode
+              key={childId}
+              label={label}
+              pos={{ x: xPos, y: 100 }}
+            />
+          );
+          xPos += nodeSpacing;
           drawn[label] = true;
         }
       }
--- src/components/node.tsx
+++ src/components/node.tsx
@@ -3,12 +3,20 @@ import './node.scss';
 import * as React from 'react';
 import Draggable from 'react-draggable';
 
-export interface ResourceNodeProps { label: string};
+export interface NodePosition {
+  x: number;
+  y: number;
+}
+
+export interface ResourceNodeProps {
+  label: string;
+  pos?: NodePosition;
+}
 
 export class ResourceNode extends React.Component<ResourceNodeProps, {}> {
   render() {
     return (
-      <Draggable>
+      <Draggable position={this.props.pos}>
         <div className="card">
           <div className="header">
             <h2>*</h2>
